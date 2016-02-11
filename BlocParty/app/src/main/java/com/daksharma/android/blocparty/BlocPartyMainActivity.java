344671d1c6eddd6e4225139e931b31d05b28fa02
Daksh Sharma <dashatrix@gmail.com>
2016-02-10 23:07:47 -0800

    BlocParty Inital Commit

---
+++ BlocParty/app/src/main/java/com/daksharma/android/blocparty/BlocPartyMainActivity.java
@@ -0,0 +1,13 @@
+package com.daksharma.android.blocparty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlocPartyMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bloc_party_main_activity);
+    }
+}
